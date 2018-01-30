90d3f0285aa87441932068566e9c7b86d698c674
Energy <energystar.0124@gmail.com>
2018-01-30 21:28:01 +0800

    Initial commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+Instructions to compile and run the program
+1. run javacc Asgn1.jj
+2. run javac *.java
+3. run java MiniJavaParser < InputJavaSourceFile
---
+++ testcase/Test.java
@@ -0,0 +1,34 @@
+/**/*/**/
+/**
+ * 
+ * @author Energy
+ *
+ */
+
+class Test@{
+    public static void main(String[] a){
+    */
+    0xKKKK
+    0ABC
+    ' "'
+	System.out.println(new BS().Start(20));
+	System.out.println(0xAF10A);
+	System.out.println(05423);
+	System.out.println(123);
+	System.out.println(0199);
+	characters
+	0x77KKKD
+	1277JIDO@EE
+    }
+    
+}
+
+
+
+/*
+ */
+
+
+// This class contains an array of integers and
+// methods to initialize, print and search the array
+// using Binary Search
